623a37cc2f0697c89a685967b16d9383af84b120
teukuikhyar <tikhyarf@gmail.com>
2026-09-13 03:14:24 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// ==========================================
+// KONFIGURASI SMART ATTENDANCE
+// ==========================================
+
+// Masukkan URL Web App Google Apps Script
+// setelah kita membuat Google Sheets + Apps Script.
+
+const API_URL =
+    "MASUKKAN_URL_GOOGLE_APPS_SCRIPT_DI_SINI";
+
+
+// ==========================================
+// MODE DEMO
+// ==========================================
+
+// true  = menggunakan data demo/localStorage
+// false = menggunakan Google Sheets
+
+const DEMO_MODE = true;
