9ed0d4d496c659736d47b0ea50a65687b10bc83f
cyberpon159 <teukuikhyar14@gmail.com>
2026-09-16 00:50:10 +0700

    Update API URL Google Apps Script

--- js/config.js
+++ js/config.js
@@ -1,3 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzWWNjij2MPfayamk2oGLChNtOyOSCk3BtTHyUqq9K0bWlNpoKSnaAfgJQXcMlnPnsu/exec";
-
+const API_URL = "https://script.google.com/macros/s/AKfycbyWN8kAAQ1HOymw8sVQxuejV7-X3grmQUJ9wHVEdmb0ySMy7uOpBHIw0QhGt9pJ8Wyu/exec";
 const DEMO_MODE = false;
